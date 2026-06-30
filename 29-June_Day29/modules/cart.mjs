4af879d71cbeb2336467a8029598679b170650df
Shravani Vikas Chikhale <shravanichikhale25@gmail.com>
2026-06-30 19:07:46 +0530

    Add files via upload

---
+++ 29-June_Day29/modules/cart.mjs
@@ -0,0 +1,11 @@
+// import details from "./product.js"
+import a from "./product.js"
+import {product,show} from "./product.js"
+
+console.log("cart Page");
+
+console.log(product);
+show();
+a();
+
+
